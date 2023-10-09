ea4e16bda42b6a8d23dc8d59e1cd1da5935aaee2
Claudio Olivera <claudioolivera753@gmail.com>
2023-10-09 19:12:57 -0300

    intento de solucion para netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  integrations: [tailwind(), react()],
   adapter: netlify(),
-  integrations: [tailwind(), react()]
 });
